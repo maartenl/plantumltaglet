1585c1b3f39eb6538b1800803ce0dfa5a0481c5d
Maarten van Leunen <m.vanleunen@gouwit.nl>
2023-10-02 14:01:52 +0200

    created a derivative of the standard javadoc doclet. Doesn't do anything differently.

---
+++ src/main/java/org/example/PlantumlDoclet.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import jdk.javadoc.doclet.StandardDoclet;
+
+/**
+ * A Doclet made by me to convert appropriate Plantuml codes into generated diagrams.
+ *
+ */
+public class PlantumlDoclet extends StandardDoclet {
+}
---
+++ src/test/java/org/example/PlantumlDocletTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Unit test for simple App.
+ */
+public class PlantumlDocletTest {
+
+  /**
+   * Rigourous Test :-)
+   */
+  public void testApp() {
+  }
+}
